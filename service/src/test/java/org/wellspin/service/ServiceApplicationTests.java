b6ead50ab0c45ad95cff661bf5ec57f3b1ba17f5
arinkadakia <kadakiaarin@gmail.com>
2022-12-04 18:27:42 -0800

    Code cleanup

--- service/src/test/java/org/wellspin/service/ServiceApplicationTests.java
+++ service/src/test/java/org/wellspin/service/ServiceApplicationTests.java
@@ -1,9 +1,13 @@
 package org.wellspin.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.wellspin.backend.ServiceApplication;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ServiceApplication.class)
 class ServiceApplicationTests {
 
 	@Test
