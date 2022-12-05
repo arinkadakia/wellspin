9fd87f9c40ebba0965ed8bc58beacd2cc0bbfc82
arinkadakia <kadakiaarin@gmail.com>
2022-12-04 18:31:02 -0800

    AWS deployment related tools

---
+++ tools/connect-aws.sh
@@ -0,0 +1,1 @@
+ssh -i "ws-keypair.pem" ec2-user@wellspin.org
---
+++ tools/scp-aws-jar.sh
@@ -0,0 +1,1 @@
+scp -i "ws-keypair.pem" $HOME/git/wellspin/3.0/service/target/service-1.0.jar ec2-user@wellspin.org:/home/ec2-user/wellspin/jar
